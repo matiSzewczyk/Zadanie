56b453cb209be053d33c49865eb45fc54d748f97
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-06-10 21:36:25 +0200

    Add login fragment

--- app/src/main/java/com/example/zadanie/LoginFragment.kt
+++ app/src/main/java/com/example/zadanie/LoginFragment.kt
@@ -1,6 +1,22 @@
 package com.example.zadanie
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.zadanie.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment(R.layout.fragment_first){
+class LoginFragment : Fragment(R.layout.fragment_login){
+
+    private lateinit var binding: FragmentLoginBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.apply {
+            loginButton.setOnClickListener {
+                // login
+            }
+        }
+    }
 }
